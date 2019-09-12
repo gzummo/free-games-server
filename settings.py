679bddd41284329a73fe3adb2d0dd54f8fcaa9a9
gzummo <7153663+gzummo@users.noreply.github.com>
2019-09-12 13:17:41 +0000

    First commit.

---
+++ settings.py
@@ -0,0 +1,9 @@
+# MONGODB
+MONGO_URI = 'mongodb://localhost:27027/'
+MONGO_DB = 'free_games'
+
+# REDIS
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6389
+REDIS_DB = 0
+REDIS_URL = 'redis://localhost:6389/0'
